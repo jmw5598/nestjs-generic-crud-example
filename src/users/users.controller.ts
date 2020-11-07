db3f63a5a69fc85f0d2547abb6681720f205e0ec
Jason White <jmw5598@gmail.com>
2020-11-07 07:56:38 -0500

    fixed file name

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
-import { AbstractCrudController } from '../common/controllers/abstract-crud.controllers';
+import { AbstractCrudController } from '../common/controllers/abstract-crud.controller';
 
 @Controller('users')
 export class UsersController extends AbstractCrudController<User, number> {
